78d89ec7eb79391b6d3d260288da5aa4ac6c56c4
Joel Christner <joel.christner@gmail.com>
2026-04-23 16:18:33 -0700

    Add hello world C# program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+/// Arrrrr
+/// <summary>
+/// Provides the entry point for the hello world program.
+/// </summary>
+internal static class Program
+{
+    /// <summary>
+    /// Writes hello world to the console.
+    /// </summary>
+    private static void Main()
+    {
+        System.Console.WriteLine("hello world");
+    }
+}
